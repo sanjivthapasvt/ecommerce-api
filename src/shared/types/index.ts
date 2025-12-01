31d5479035d7b1e96735a6d4110b0b31ed275c78
Sanjiv Thapa <thapasvt12@gmail.com>
2025-12-01 20:20:15 +0545

    ✅ Created new type Authenticated Request

--- src/shared/types/index.ts
+++ src/shared/types/index.ts
@@ -1,5 +1,19 @@
+import { Request } from "express";
+
 export type TEmailSendType = {
   to: string | string[];
   subject: string;
   text: string;
 };
+
+export interface AuthenticatedRequest<
+  P = {},
+  ResBody = any,
+  ReqBody = any,
+  ReqQuery = {}
+> extends Request<P, ResBody, ReqBody, ReqQuery> {
+  user: {
+    id: number;
+    email: string;
+  };
+}
